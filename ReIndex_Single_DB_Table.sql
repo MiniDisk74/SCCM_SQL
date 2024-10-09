1870e84f4758b4f258939e210c39ac525a5ae9ab
MiniDisk74 <minidiskowns@hotmail.com>
2024-10-09 16:24:03 -0500

    ReIndex a Single Table instead of a Entire DB

---
+++ ReIndex_Single_DB_Table.sql
@@ -0,0 +1,7 @@
+
+--Reindex the tables 1 by 1DBCC DBREINDEX ('DepPolicyAssignment','',80)  
+DBCC DBREINDEX ('StatusMessageAttributes','',80)  
+
+ 
+--Update stats on same tables 1 by 1
+UPDATE STATISTICS StatusMessageAttributes WITH FULLSCAN
